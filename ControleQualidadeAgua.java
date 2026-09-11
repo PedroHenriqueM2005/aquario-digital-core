7633f077f05e31f7e670aaf104341605daa84ec6
Pedro Henrique Martins <156636816+PedroHenriqueM2005@users.noreply.github.com>
2026-09-11 20:26:04 -0300

    Remove redundant whitespace in ControleQualidadeAgua

--- ControleQualidadeAgua.java
+++ ControleQualidadeAgua.java
@@ -7,20 +7,19 @@ public class ControleQualidadeAgua {
         this.ph = ph;
         this.temperatura = temperatura;
     }
+public boolean verificarParametros() {
 
-    public boolean verificarParametros() {
-
-        if (ph < 6.8 || ph > 7.6) {
+    if (ph < 6.8 || ph > 7.6) {
             System.out.println("ALERTA QA: Nível de pH fora do limite ideal!");
             return false;
         }
 
-        if (temperatura < 22.0 || temperatura > 28.0) {
+    if (temperatura < 22.0 || temperatura > 28.0) {
             System.out.println("ALERTA QA: Temperatura fora do limite seguro!");
             return false;
         }
 
-        System.out.println("STATUS: Parâmetros da água em níveis ideais.");
+    System.out.println("STATUS: Parâmetros da água em níveis ideais.");
         return true;
     }
 }
